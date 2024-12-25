1d8c067a7c689c3e9b489b4db4c1d99a8ebee46b
Burak Karaman <karaman.buraak@gmail.com>
2024-12-25 18:48:51 +0100

    [feat] Update project & Add data.json file

--- consumer.js
+++ consumer.js
@@ -1,16 +1,26 @@
 const amqp = require("amqplib");
 
+const queueName = process.argv[2] || "jobsQueue";
+const data = require("./data.json");
+
 connect_to_rabbitmq();
 
 async function connect_to_rabbitmq() {
   try {
     const connection = await amqp.connect("amqp://localhost:5672");
     const channel = await connection.createChannel();
-    const assertion = channel.assertQueue("jobsQueue");
+    const assertion = channel.assertQueue(queueName);
 
     // Empfang der Nachrict...
-    channel.consume("jobsQueue", (message) => {
-      console.log("Message", message.content.toString());
+    console.log("Nachricht erwartet...");
+
+    channel.consume(queueName, (message) => {
+      const messageInfo = JSON.parse(message.content.toString());
+      const userInfo = data.find((u) => u.id == messageInfo.description);
+      if (userInfo) {
+        console.log("Nachricht empfangen", userInfo);
+        channel.ack(message);
+      }
     });
   } catch (error) {
     console.log("Error", error);
--- publisher.js
+++ publisher.js
@@ -4,16 +4,31 @@ const message = {
   description: "Das ist eine Testnachricht",
 };
 
+const data = require("./data.json");
+const queueName = process.argv[2] || "jobsQueue";
+// console.log("queue", queueName);
+
 connect_to_rabbitmq();
 
 async function connect_to_rabbitmq() {
   try {
     const connection = await amqp.connect("amqp://localhost:5672");
     const channel = await connection.createChannel();
-    const assertion = channel.assertQueue("jobsQueue");
+    const assertion = channel.assertQueue(queueName);
 
-    channel.sendToQueue("jobsQueue", Buffer.from(JSON.stringify(message)));
-    console.log("Nachricht gesendet", message);
+    data.forEach((i) => {
+      message.description = i.id;
+      channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)));
+      console.log("Nachricht gesendet", i.id);
+    });
+
+    // ================================== Interval ==========================
+    // setInterval(() => {
+    //     message.description = new Date().getTime();
+    //     channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)));
+    //     console.log("Nachricht gesendet", message);
+    // }, 10);
+    // ================================== Interval ==========================
   } catch (error) {
     console.log("Error", error);
   }
